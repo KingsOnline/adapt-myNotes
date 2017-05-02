0508cbe0b9252ca34b4c3d0cbee8ff82b4ced29b
simondate <simondate@yahoo.co.uk>
2017-05-02 16:58:50 +0100

    Initial commit of working version of the extension. Improvements need to be made to interaction with the Moodle extension but seems OK

--- js/adapt-myNotes.js
+++ js/adapt-myNotes.js
@@ -1,33 +1,69 @@
 define(function(require) {
 
-    var Adapt = require('coreJS/adapt');
-    var Backbone = require('backbone');
-    var myNotesView = require('extensions/adapt-myNotes/js/adapt-myNotesView');
+  var Adapt = require('coreJS/adapt');
+  var Backbone = require('backbone');
+  var myNotesView = require('extensions/adapt-myNotes/js/adapt-myNotesView');
 
-    Adapt.on("pageView:ready", function() {
-        loadMyNotes();
+  Adapt.once('pageView:ready', function() {
+    if (!Adapt.iframe) {
+      removeIframeHolder();
+      createIframeHolder();
+    }
+    createMyNotes();
+    loadMyNotes();
+  });
+
+  Adapt.on("menuView:ready", function() {
+    removeElements();
+  });
+
+  function loadMyNotes() {
+    console.log(Adapt.course);
+    if (Adapt.course.attributes._myNotes._isEnabled === true) {
+      new myNotesView({
+        model: new Backbone.Model()
+      });
+    }
+  }
+
+  function createMyNotes() {
+    console.log('createnotes');
+    $('.moodle-iframe-holder').append("<iframe name='myNotesIframe' id='myNotesIframe' class=myNotes-iframe src='" + Adapt.course.attributes._myNotes._link + "'></iframe>");
+  }
+
+
+  function createIframeHolder() {
+    console.log('created iframe holder');
+    $('html').append("<div class='moodle-view close'><button class='moodle-close-button'></button><div class='moodle-iframe-holder'></div></div>");
+    $('body').addClass('moodle-close');
+    applyCSSFile();
+    Adapt.iframe = true;
+  }
+
+  function applyCSSFile() {
+    $('.moodle-iframe-holder').addClass('loading-iframe');
+    $('.myNotes-iframe').on('load', function() {
+      var adaptCSS = location.protocol + '//' + location.host + location.pathname;
+      adaptCSS = adaptCSS.substring(0, adaptCSS.lastIndexOf('/'));
+      adaptCSS += "/assets/adapt-myNotes.css"
+      console.log('APPENDING TO HEAD ' + adaptCSS);
+      $('.moodle-iframe').contents().find("head").append($("<link/>", {
+        rel: "stylesheet",
+        href: adaptCSS,
+        type: "text/css"
+      }));
+
+      document.getElementById('moodleIframe').contentWindow.window.onbeforeunload = null; // prevents error message when leaving moodle page when you haven't submitted.
+
+      setTimeout(function() {
+        $('.moodle-iframe-holder').removeClass('loading-iframe');
+      }, 100);
     });
+  }
 
-    Adapt.on("menuView:ready", function() {
-        removeElements();
-    });
+  function removeIframeHolder() {
+    $('.moodle-view').remove();
+    $('body').removeClass('moodle-close moodle-open');
+  }
 
-    function loadMyNotes() {
-        console.log(Adapt.course);
-        if (Adapt.course.attributes._myNotes._isEnabled === true) {
-          new myNotesView({
-              model: new Backbone.Model()
-          });
-        }
-    }
-
-    function createElements() {
-        $('html').append("<div class='moodle-view close'><button class='moodle-close-button'></button><div class='moodle-iframe-holder'></div></div>");
-        $('body').addClass('moodle-close');
-    }
-
-    function removeElements() {
-        $('.moodle-view').remove();
-        $('body').removeClass('moodle-close moodle-open');
-    }
 });
--- js/adapt-myNotesView.js
+++ js/adapt-myNotesView.js
@@ -1,121 +1,48 @@
 define(function(require) {
 
-    var Adapt = require('coreJS/adapt');
-    var Backbone = require('backbone');
+  var Adapt = require('coreJS/adapt');
+  var Backbone = require('backbone');
 
-    var moodleView = Backbone.View.extend({
+  var myNotesView = Backbone.View.extend({
 
-        className: "myNotes",
+    className: "myNotes",
 
-        initialize: function(blockModel) {
-            console.log('init');
-            this.listenTo(Adapt, 'remove', this.remove);
-            var data = this.model.toJSON();
-            var template = Handlebars.templates.myNotes;
-            this.setElement(template(data)).$el.appendTo($(".navigation-inner"));
-            this.listenTo(Adapt, {
-                "navigation:openMyNotes": this.helloWorld
-            });
-        },
+    initialize: function(blockModel) {
+      console.log('init');
+      this.listenTo(Adapt, 'remove', this.remove);
+      var data = this.model.toJSON();
+      var template = Handlebars.templates.myNotes;
+      this.setElement(template(data)).$el.appendTo($(".navigation-inner"));
+      this.listenTo(Adapt, {
+        "navigation:openMyNotes": this.launchButton
+      });
+    },
 
-        events: {
-            'click button.navigation-myNotes': 'helloWorld'
-        },
+    launchButton: function(event) {
+      $('.myNotes-iframe').css('display', 'block');
+      $('.myNotes-iframe').siblings().css('display', 'none');
+      if ($('.moodle-view').hasClass('open')) {
+        this.closeLightbox(event); // close
+      } else {
+        this.openLightbox(event);
+      }
+    },
 
-        helloWorld: function(event) {
-          console.log('hello world');
-        },
+    openLightbox: function(event) {
+      $('body').addClass('moodle-open').removeClass('moodle-close');
+      $('.moodle-view').removeClass('close').addClass('open');
+      $('.moodle-close-button').text('Close My Notes');
+      var context = this;
+      $(".moodle-close-button").on("click", function() {
+        context.closeLightbox();
+      });
+    },
 
-        launchButton: function(event) {
-            if ($(event.target).hasClass('open')) {
-                this.closeLightbox(event); // close
-            } else {
-                this.openLightbox(event);
-            }
-        },
+    closeLightbox: function(event) {
+      $('.moodle-view').removeClass('open').addClass('close');
+      $('body').removeClass('moodle-open').addClass('moodle-close');
+    }
+  });
 
-        openLightbox: function(event) {
-            $('.moodle-launch-button.open').removeClass('open'); // closes other instances
-            $(event.target).addClass('open');
-            $('body').addClass('moodle-open').removeClass('moodle-close');
-            $('.moodle-view').removeClass('close').addClass('open');
-
-            var linkToBlock = location.protocol + '//' + location.host + location.pathname + '#/id/' + this.attributes._id;
-            if ($('.moodle-iframe').attr('src') != this.attributes._moodle._link) {
-                $('.moodle-iframe-holder').children().remove();
-                this.renderIframe(this.attributes._moodle._type, linkToBlock);
-            }
-
-            this.scrollToBlock();
-            this.setupCloseButton();
-
-        },
-
-        scrollToBlock: function() {
-            var $firstChild = '.' + this.attributes._children.models[0].attributes._id;
-            Adapt.scrollTo($($firstChild), {
-                duration: 700
-            });
-        },
-
-        setupCloseButton: function() {
-            $('.moodle-close-button').html("Close " + this.attributes._moodle.buttonLabel);
-            var context = this;
-            $(".moodle-close-button").on("click", function() {
-                context.closeLightbox();
-            });
-        },
-
-        renderIframe: function(type, linkToBlock) {
-            $('.moodle-iframe-holder').addClass('loading-iframe');
-            $('.moodle-iframe-holder').append("<iframe name='moodleIframe' id='moodleIframe' class='moodle-iframe'></iframe>");
-            $('.moodle-iframe').attr('src', this.attributes._moodle._link);
-
-            $('.moodle-iframe').on('load', function() {
-                var adaptCSS = location.protocol + '//' + location.host + location.pathname;
-                adaptCSS = adaptCSS.substring(0, adaptCSS.lastIndexOf('/'));
-                adaptCSS += "/assets/adapt-moodle-iframe-" + type + ".css"
-                console.log('APPENDING TO HEAD ' + adaptCSS);
-                $('.moodle-iframe').contents().find("head").append($("<link/>", {
-                    rel: "stylesheet",
-                    href: adaptCSS,
-                    type: "text/css"
-                }));
-
-                document.getElementById('moodleIframe').contentWindow.window.onbeforeunload = null; // prevents error message when leaving moodle page when you haven't submitted.
-
-                setTimeout(function() {
-                    $('.moodle-iframe-holder').removeClass('loading-iframe');
-                }, 100);
-            });
-        },
-
-        closeLightbox: function(event) {
-            console.log('close')
-            if (event && event.preventDefault) event.preventDefault();
-            $('.moodle-view').removeClass('open').addClass('close');
-            $('.moodle-launch-button.open').removeClass('open');
-            $('body').removeClass('moodle-open').addClass('moodle-close');
-        },
-
-        setButtonName: function() {
-          if(this.attributes._moodle.buttonLabel == undefined || this.attributes._moodle.buttonLabel == "") {
-            this.attributes._moodle.buttonLabel = this.attributes._moodle._type;
-          }
-        },
-
-        render: function(blockModel) {
-            // // Convert model data into JSON
-            this.setButtonName();
-            var data = blockModel.toJSON();
-            data = data._moodle;
-            var template = Handlebars.templates["moodle"];
-            var $selector = $('.' + blockModel.attributes._id + '>:first');
-            this.$el.html(template(data)).appendTo($selector);
-            return this;
-        }
-    });
-
-    // Return moodleView so it can be required
-    return moodleView;
+  return myNotesView;
 });
